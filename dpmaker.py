8f333ab46f7409609bcb6ed49dd1e53ac5bbfd38
Kartikey Rai <61941790+kartikeyarai7@users.noreply.github.com>
2020-08-11 18:02:35 +0530

    Add files via upload
    For keeping the aspect ratio

--- dpmaker.py
+++ dpmaker.py
@@ -5,8 +5,8 @@ user = input('Where do you want to use this picture \n 1. Whatsapp 2.Instagram 3
 def img_converter(user):
     if user == "1":
         img = Image.open("./meee.jpg")  #INSERT YOUR IMAGE IN THE SAME DIRECTORY AND SAVE IT AS meee.jpg
-        ws_dp = img.resize((192,192))
-        bws_dp = ws_dp.convert("L")
+        img.thumbnail((192,192))  #Thumbnail keeps the aspect ratio
+        bws_dp = img.convert("L")
         # beautified_dp = bws_dp.filter(ImageFilter.SMOOTH) IF YOU WANT TO SMOOTHEN IT, UNCOMMENT THIS
         # bws_dp.save("Wsdpbw.png","png") IF YOU WANT TO SAVE UNCOMMENT THIS
         # print(beautified_dp)
@@ -15,8 +15,8 @@ def img_converter(user):
 
     if user == "2":
         img = Image.open("./meee.jpg")
-        ig_dp = img.resize((110,110))
-        bws_dp = ig_dp.convert("L")
+        img.thumbnail((110,110))
+        bws_dp = img.convert("L")
         # beautified_dp = bws_dp.filter(ImageFilter.SMOOTH) IF YOU WANT TO SMOOTHEN IT, UNCOMMENT THIS
         # bws_dp.save("Wsdpbw.png","png") IF YOU WANT TO SAVE UNCOMMENT THIS
         # print(beautified_dp)
@@ -25,8 +25,8 @@ def img_converter(user):
 
     if user == "3":
         img = Image.open("./meee.jpg")
-        fb_dp = img.resize((170,170))
-        bws_dp = fb_dp.convert("L")
+        img.thumbnail((170,170))
+        bws_dp = img.convert("L")
         # beautified_dp = bws_dp.filter(ImageFilter.SMOOTH) IF YOU WANT TO SMOOTHEN IT, UNCOMMENT THIS
         # bws_dp.save("Wsdpbw.png","png") IF YOU WANT TO SAVE UNCOMMENT THIS
         # print(beautified_dp)
